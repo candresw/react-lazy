bec45b78d191fa796478643c8eb7fe3697d38739
Andrés Wellmann <dev3@gruporealcaruz.com>
2022-01-31 10:23:35 -0600

    Lazy loading en rutas de react listo

---
+++ src/routes/routes.ts
@@ -0,0 +1,37 @@
+import { lazy, LazyExoticComponent } from 'react';
+// import { LazyPage, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+type JSXComponent = () => JSX.Element;
+
+interface Route {
+    to: string;
+    path: string;
+    name: string;
+    Component: LazyExoticComponent<() => JSX.Element> | JSXComponent;
+}
+
+const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage'));
+const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
+const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+
+
+export const routes: Route[] = [
+    {
+        to: '/lazy',
+        path: 'lazy',
+        Component: Lazy1,
+        name: 'Lazy' 
+    },
+    {
+        to: '/lazy2',
+        path: 'lazy2',
+        Component: Lazy2,
+        name: 'Lazy2' 
+    },
+    {
+        to: '/lazy3',
+        path: 'lazy3',
+        Component: Lazy3,
+        name: 'Lazy3' 
+    },
+]
