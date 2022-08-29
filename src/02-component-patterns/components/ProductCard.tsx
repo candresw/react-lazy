447015a59e468d1319415636b1658e4eca11a45f
Andrés Wellmann <dev3@gruporealcaruz.com>
2022-08-29 16:05:05 -0600

    Product Component Pattern

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,63 +1,29 @@
-import React from 'react';
+import React, { createContext } from 'react';
+
 import { useProduct } from '../hooks/useProduct';
+import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+
 import styles from '../styles/styles.module.css';
-import noImage from '../assets/no-image.jpg';
 
-interface Props {
-  product: Product
-}
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
 
-interface Product {
-  id: string;
-  title: string;
-  img?: string;
-}
 
-export const ProductImage = ({ img = '' }) => {
-  return (
-    <img className={ styles.productImg } src={img ? img : noImage } alt="Product" />
-  )
-}
-
-export const ProductTitle = ({ title }: { title: string }) => {
-  return(
-    <span className={ styles.productDescription }>{ title }</span>
-  )
-}
-
-interface ProductButtonsProps {
-  increaseBy: (value: number) => void;
-  counter: number;
-}
-
-export const ProductButtons = ({ increaseBy, counter }: ProductButtonsProps ) => {
-  return(
-    <div className={ styles.buttonsContainer }>
-      <button className={ styles.buttonMinus} onClick={ () => increaseBy( -1 ) } >-</button>
-
-      <div className={ styles.countLabel }> { counter } </div>
-
-      <button className={ styles.buttonAdd } onClick={ () => increaseBy( +1 ) } >+</button>
-    </div>
-  )
-}
-
-export const ProductCard = ({product}: Props) => {
+export const ProductCard = ({children, product}: ProductCardProps) => {
 
   const { counter, increaseBy } = useProduct();
 
   return (
-    <div className={ styles.productCard } >
+    <Provider value={{
+      counter,
+      increaseBy,
+      product
+    }}>      
+      <div className={ styles.productCard } >
 
-      <ProductImage img={ product.img } />
+        { children }
 
-      <ProductTitle title={ product.title } />
-
-      <ProductButtons 
-        increaseBy={ increaseBy } 
-        counter={ counter }  
-      />
-
-    </div>
+      </div>
+    </Provider>
   )
 }
